f871142edd284bc47011e288d517c7a81ddf7225
imalbert <albert.s.manuel@gmail.com>
2020-05-12 15:11:09 +0800

    www: update footer, add linkedin

--- packages/www/src/components/footer.js
+++ packages/www/src/components/footer.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 const Footer = () => (
-  <footer className="container mx-auto flex-none">
-    Built with
-    {` `}
-    <a href="https://www.gatsbyjs.org" target="_blank">Gatsby</a>{` `}
+  <footer className="container mx-auto flex justify-end p-12 space-x-2 text-base">
+    <a href="https://www.gatsbyjs.org" target="_blank">
+      Built with Gatsby
+    </a>
     <a href="https://github.com/imalbert" target="_blank">
-      by @imalbert {` `}
-      <FontAwesomeIcon icon={faGithub} />
-    </a> {` `}
+      <FontAwesomeIcon icon={faGithub} size="lg" />
+    </a>
+    <a href="https://www.linkedin.com/in/albertsmanuel/" target="_blank">
+      <FontAwesomeIcon icon={faLinkedin} size="lg" />
+    </a>
     <a href="mailto:albert.s.manuel@gmail.com">
-      Send me an email!
+      <FontAwesomeIcon icon="envelope" size="lg" />
     </a>
   </footer>
 )
