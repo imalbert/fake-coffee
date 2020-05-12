0127a537072509c4ff8dc3b8a683f92ff36ae424
imalbert <albert.s.manuel@gmail.com>
2020-05-12 15:17:32 +0800

    www: small adjustment on footer, justify-between

--- packages/www/src/components/footer.js
+++ packages/www/src/components/footer.js
@@ -3,19 +3,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 const Footer = () => (
-  <footer className="container mx-auto flex justify-end p-12 space-x-2 text-base">
-    <a href="https://www.gatsbyjs.org" target="_blank">
-      Built with Gatsby
-    </a>
-    <a href="https://github.com/imalbert" target="_blank">
-      <FontAwesomeIcon icon={faGithub} size="lg" />
-    </a>
-    <a href="https://www.linkedin.com/in/albertsmanuel/" target="_blank">
-      <FontAwesomeIcon icon={faLinkedin} size="lg" />
-    </a>
-    <a href="mailto:albert.s.manuel@gmail.com">
-      <FontAwesomeIcon icon="envelope" size="lg" />
-    </a>
+  <footer className="container mx-auto flex justify-between p-12 text-base">
+    <div>
+      <a href="https://www.gatsbyjs.org" target="_blank">
+        # 2020 Built with Gatsby
+      </a>
+    </div>
+    <div className="space-x-6">
+      <a href="https://github.com/imalbert" target="_blank">
+        <FontAwesomeIcon icon={faGithub} size="lg" />
+      </a>
+      <a href="https://www.linkedin.com/in/albertsmanuel/" target="_blank">
+        <FontAwesomeIcon icon={faLinkedin} size="lg" />
+      </a>
+      <a href="mailto:albert.s.manuel@gmail.com">
+        <FontAwesomeIcon icon="envelope" size="lg" />
+      </a>
+    </div>
   </footer>
 )
 
