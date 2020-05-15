bc4ae7e4a886a671aea97db720860fc9d2c8102a
imalbert <albert.s.manuel@gmail.com>
2020-05-15 15:38:06 +0800

    www: new coffee/* page

--- packages/www/src/components/text/heading1.js
+++ packages/www/src/components/text/heading1.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Heading1 = ({ children }) => {
   return (
-    <h1 className="font-serif text-6xl font-medium tracking-tighter italic">
+    <h1 className="font-serif text-5xl font-semibold tracking-tighter italic">
       {children}
     </h1>
   )
--- packages/www/src/templates/coffee-page.js
+++ packages/www/src/templates/coffee-page.js
@@ -1,8 +1,12 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+
+import Hyperlink from "../components/hyperlink"
 import HeaderLink from "../components/header/header-link"
+import HeaderButton from "../components/header/header-button"
 import Icon from "../components/icon/icon"
 import Text from "../components/text"
 
@@ -18,30 +22,116 @@ export default function BlogPost({ data }) {
 
   return (
     <div className="bg-primary">
-      <div className="container mx-auto border">
+      <div className="container mx-auto">
         <div className="flex bg-white">
-          <aside className="">
+          <aside className="hidden sm:block p-4 sm:mt-4">
             <HeaderLink>
-              <Icon icon="coffee" size="lg" className="text-secondary-light" />
+              <Icon icon="coffee" size="2x" className="text-secondary-light" />
             </HeaderLink>
           </aside>
 
-          <main className="flex-grow">
-            <header></header>
-
-            <section className="relative my-16 py-16">
-              <div className="">
-                <Text.Heading1>Fake Coffee</Text.Heading1>
+          <main className="flex-grow flex flex-col">
+            <header className="flex p-4 sm:mt-6 space-x-4">
+              <div className="flex w-1/4 items-center space-x-1">
+                <Icon icon="map-marker" className="text-secondary-light" />
+                <span
+                  className="tracking-tight leading-3"
+                  style={{ fontSize: "0.5rem" }}
+                >
+                  Lorem ipsum dolor, sit amet, 74903-4
+                </span>
               </div>
+              <div className="flex w-1/4 items-center space-x-1">
+                <Icon icon="paper-plane" className="text-secondary-light" />
+                <span
+                  className="text-xs tracking-tight leading-3"
+                  style={{ fontSize: "0.5rem" }}
+                >
+                  +63 999-999-999
+                  <br />
+                  +63 999-999-999
+                </span>
+              </div>
+              <div className="flex w-1/2 items-center space-x-1">
+                <div class="relative w-full shadow-sm">
+                  <input
+                    id="search"
+                    class="form-input block p-2 sm:text-sm sm:leading-5"
+                  />
+                  <div class="mr-2 absolute inset-y-0 right-0 flex items-center">
+                    <Icon icon="search" className="text-secondary-light" />
+                  </div>
+                </div>
+              </div>
+            </header>
 
-              <figure className="sm:absolute sm:right-0 sm:top-0 sm:-mr-32 sm:mt-16">
-                <Img fluid={sources} className="w-64" />
-              </figure>
-            </section>
+            <div className="p-4 sm:p-0 flex-grow flex flex-col items:center">
+              <section className="relative m-0 sm:py-16">
+                <div className="text-center sm:text-left space-y-4">
+                  <Text.Heading1>Fake Coffee</Text.Heading1>
+                  <figure className="sm:-mr-32 sm:right-0 sm:top-0 z-10 sm:absolute">
+                    <Img fluid={sources} className="w-40 sm:w-64 mx-auto" />
+                  </figure>
+                  <p className="mx-auto sm:mx-0 w-64 text-xs leading-loose">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+
+                  <div className="inline-block">
+                    <Link
+                      to="/"
+                      className="text-xs tracking-tight italic font-serif font-semibold border-black border-b inline-block"
+                    >
+                      Read more
+                    </Link>
+                  </div>
+
+                  <div className="flex justify-center sm:justify-start items-baseline space-x-4">
+                    <Text.Heading1>$0</Text.Heading1>
+                    <div className="w-8 h-8 rounded-full bg-primary flex items-center justify-center">
+                      <Icon icon="plus" className="text-white" />
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
           </main>
 
-          <aside className="flex h-screen bg-primary">
-            <nav className="hidden sm:block w-64"></nav>
+          <aside className="fixed bottom-0 right-0 sm:relative z-0 w-0 sm:w-1/3 bg-primary h-screen">
+            <nav className="p-4 m-4 space-x-2 hidden sm:flex justify-end items-center right-0">
+              <Hyperlink href="https://github.com/imalbert">
+                <Icon icon={faGithub} className="text-white" />
+              </Hyperlink>
+              <Hyperlink href="https://www.linkedin.com/in/albertsmanuel/">
+                <Icon icon={faLinkedin} className="text-white" />
+              </Hyperlink>
+              <Hyperlink href="mailto:albert.s.manuel@gmail.com">
+                <Icon icon="envelope" className="text-white" />
+              </Hyperlink>
+
+              <div className="pl-4 space-x-4 items-center flex">
+                <Icon icon="portrait" className="text-white" />
+                <HeaderButton
+                // onClick={() => toggleBurger(!burgerOpen)}
+                >
+                  <Icon icon="hamburger" size="lg" className="text-white" />
+                </HeaderButton>
+              </div>
+            </nav>
+
+            <nav className="mr-4 mb-4 sm:mr-8 sm:mb-8 w-12 h-12 bg-primary sm:bg-white bottom-0 right-0 rounded-full flex items-center justify-center absolute">
+              <div className="relative">
+                <Icon
+                  icon="shopping-cart"
+                  size="lg"
+                  className="text-white sm:text-primary m-1"
+                />
+                <span className="-mt-3 -mr-3 w-5 h-5 text-primary sm:text-white bg-white sm:bg-primary text-xs font-semibold border border-primary sm:border-white rounded-full flex items-center justify-center top-0 right-0 absolute">
+                  3
+                </span>
+              </div>
+            </nav>
           </aside>
         </div>
       </div>
