fde6923b9e6abb6112dd34c5383ce4be17e6d09e
imalbert <albert.s.manuel@gmail.com>
2020-05-15 09:52:57 +0800

    www: Add coffee image assets

--- packages/www/src/templates/coffee-page.js
+++ packages/www/src/templates/coffee-page.js
@@ -1,19 +1,31 @@
 import React from "react"
 import { graphql } from "gatsby"
-import PageLayout from "../components/layout/subpage-layout"
-import CoffeeCard from "../components/card/coffee-card"
+
+import HeaderLink from "../components/header/header-link"
+import Icon from "../components/icon/icon"
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
   return (
-    <PageLayout
-      header={{
-        title: post.frontmatter.title,
-        subtitle: post.frontmatter.origin,
-      }}
-    >
-      <CoffeeCard {...post.frontmatter} />
-    </PageLayout>
+    <div className="bg-primary">
+      <div className="container mx-auto border">
+        <div className="flex bg-white">
+          <aside className="">
+            <HeaderLink>
+              <Icon icon="coffee" size="lg" className="text-secondary-light" />
+            </HeaderLink>
+          </aside>
+
+          <main className="flex-grow">
+            <header></header>
+          </main>
+
+          <aside className="flex h-screen bg-primary">
+            <nav className="hidden sm:block w-64"></nav>
+          </aside>
+        </div>
+      </div>
+    </div>
   )
 }
 
